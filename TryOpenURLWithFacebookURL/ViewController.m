4411e7746433bb2f0062063c5894df0070096e9d
AlanYen <alanyen.mobile@gmail.com>
2015-09-09 17:51:59 +0800

    Init Commit

--- TryOpenURLWithFacebookURL/ViewController.m
+++ TryOpenURLWithFacebookURL/ViewController.m
@@ -2,13 +2,15 @@
 //  ViewController.m
 //  TryOpenURLWithFacebookURL
 //
-//  Created by AlanYen on 2015/9/9.
-//  Copyright (c) 2015年 17Life. All rights reserved.
+//  Created by Alan.Yen on 2015/9/9.
+//  Copyright (c) 2015年 17Life All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -16,7 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSMutableString *text = [NSMutableString string];
+    [text appendString:@"[Open by FB APP]\n"];
+    [text appendString:@"http://www.facebook.com/suhung.fans"];
+    [text appendString:@"\n\n"];
+    [text appendString:@"[Open by Safari]\n"];
+    [text appendString:@"http://www.facebook.com/suhung.fans?"];
+    [text appendString:@"\n\n"];
+    [text appendString:@"[FB URL Schemes List]\n"];
+    [text appendString:@"http://wiki.akosma.com/IPhone_URL_Schemes#Facebook"];
+    [text appendString:@"\n\n"];
+    [text appendString:@"[Open by FB URL Schemes]\n"];
+    [text appendString:@"fb://profile/113655892026342"];
+    self.textView.text = text;
+    self.textView.editable = NO;
+    self.textView.dataDetectorTypes = UIDataDetectorTypeLink;
 }
 
 - (void)didReceiveMemoryWarning {
